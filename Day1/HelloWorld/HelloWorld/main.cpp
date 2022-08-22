542456b2b78e4c15f6758b812f94171e16631d18
Stuart <stuartjkirkham@gmail.com>
2022-08-22 12:11:13 -0600

    Committing my hello world project to git

---
+++ Day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Stuart Kirkham on 8/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! and cs6010! \n";
+    std::cout <<"\n";
+    std::cout <<2+2;
+    std::cout <<"\n";
+    return 0;
+}
