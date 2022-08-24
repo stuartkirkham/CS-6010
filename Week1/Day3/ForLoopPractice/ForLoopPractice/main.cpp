f12dc1f3d8b5763e2781ba0bf0d3ea2f2b7fc26b
Stuart <stuartjkirkham@gmail.com>
2022-08-24 13:50:42 -0600

    finished ForLoopPractice lab.

--- Week1/Day3/ForLoopPractice/ForLoopPractice/main.cpp
+++ Week1/Day3/ForLoopPractice/ForLoopPractice/main.cpp
@@ -36,12 +36,33 @@ int main() {
             firstNum = firstNum +1;
         }
     }
-        
-
 //Print all the odd numbers between 1 and 20. Come up with two solutions: one that uses a loop and an if statement, and one that doesn't require an if statement. Comment on which you think is better.
-
+    std::cout<<"\n";
+    for (int oddNum = 1; oddNum<=20;){ //I thought that this loop was easier to understand than running the if loop.
+        std::cout << oddNum <<" ";
+        oddNum=oddNum+2;
+    }
+    std::cout<<"\n";
+    for (int oddNum = 1; oddNum<=20 ; oddNum++){
+        if (oddNum % 2 == 1){
+            std::cout<< oddNum << " ";
+        }
+    }
 //Ask a user to enter positive numbers to add up. Keep reading and adding numbers until the user enters a number that is less than 0, then print the sum. For example, if the user entered 1 2 3 -1, you should print 6. This one is a little tricky!
-
+    std::cout << "\n";
+    std::cout << "Enter positive numbers to add up. to quit, enter a negative number.";
+    int addRandomNumber = 0;
+    int totalRandomNumber = 0;
+    while (addRandomNumber >=0){
+        std::cin >> addRandomNumber;
+        if (addRandomNumber>=0){
+            totalRandomNumber = totalRandomNumber + addRandomNumber;
+        }
+    }
+    std::cout<<"Total: " << totalRandomNumber << "\n";
 //Print a multiplication table for the numbers 1 to 5.
+    for (int multiplierVariable = 1; multiplierVariable <=5; multiplierVariable++){
+        std::cout <<multiplierVariable << " " << (multiplierVariable*2) << " " << (multiplierVariable*3) << " " << (multiplierVariable*4) << " " << (multiplierVariable*5) << " \n";
+    }
         return 0;
 }
