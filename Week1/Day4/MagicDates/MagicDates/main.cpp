187752be75ca5dec5fe744557595b2f0510f24a1
Stuart <stuartjkirkham@gmail.com>
2022-08-25 11:05:07 -0600

    MagicDates group lab

---
+++ Week1/Day4/MagicDates/MagicDates/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  MagicDates
+//
+//  Stu Kirkham, AnnMarie Hoppel, Josh Adams
+//
+
+#include <iostream>
+
+int main() {
+//    magic date is one in which the day multiplied by the month is equal to the last two digits of the year. For example:
+
+//   10/04/1940 is a magic date because 10 * 4 is 40.
+
+//   10/04/2040 is also a magic date because we are only concerned with the last two digits of the year.
+
+ //  Write a program that prompts the user for a date in US numeric format (mm/dd/yyyy), and reports whether or not it is a magic date. The same assumptions and error-checking as in Part 2 also apply here.
+    std::cout<< "Please enter a date in US numeric format (mm/dd/yyyy) \n";
+    std::cin >> date.string();
+    
+    std::cin >>
+    
+    return 0;
+}
