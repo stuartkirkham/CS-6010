4bce588eaea47019b1d55ba8ef5bcdec78eca368
Stuart <stuartjkirkham@gmail.com>
2022-08-26 14:13:54 -0600

    stringanalyzer homework

--- Week1/Day5/StringAnalyzer/StringAnalyzer/main.cpp
+++ Week1/Day5/StringAnalyzer/StringAnalyzer/main.cpp
@@ -9,45 +9,38 @@
 bool IsTerminator(char terminatorCounter) {
     //The bool isTerminator helps to determine how many sentences there are.
     //Returns whether or not the character is ., ?, or !.
-    if (terminatorCounter == "."){
-        return 1;
-    }
-    else if (terminatorCounter == "?"){
-        
-    }
-    else if (terminatorCounter == "!"){
-        
-    }
-    else {
-        return 0;
-    }
+    int findPeriod = std::str.find(".");
+    int findQuestionMark = std::str.find("?");
+    int findExclamationMark = std::str.find("!");
+    
+
 }
-bool IsPunctuation(char c) {
+bool IsPunctuation(char punctuationCounter) {
     //The bool isPunctuation helps to not count those punctuation marks as a part of the letter count. Because commas aren't letters, but they're also not terminators of sentences.
     //Returns whether or not the character is ., ?, !, or ,.
 }
-bool IsVowel(char c) {
+bool IsVowel(char vowelChecker) {
     // Returns whether or not the character is a vowel. For our purposes, always treat the character 'y' (and its uppercase version) as a vowel.
 }
-bool IsConsonant(char c) {
+bool IsConsonant(char consonantChecker) {
     //Returns whether or not the character is a consonant.
 }
-int NumWords(std::string s) {
+int NumWords(std::string wordCounter) {
     //Returns the number of words in the string.
 }
-int NumSentences(std::string s) {
+int NumSentences(std::string sentenceCounter) {
     //Returns the number of sentences in the string. This function can make use of the IsPunctuation helper.
 }
-int NumVowels(std::string s) {
+int NumVowels(std::string vowelCounter) {
     //Returns the number of vowels in the string. This function can make use of the IsVowel helper.
 }
-int NumConsonants(std::string s) {
+int NumConsonants(std::string consonantCounter) {
     // Returns the number of consonants in the string. This function can make use of the IsConsonant helper.
 }
-double AverageWordLength(std::string s) {
+double AverageWordLength(std::string wordLength) {
     //Returns the average length of all words in the string. This function can make use of the IsVowel , IsConsonant , and NumWords helpers.
 }
-double AverageVowelsPerWord(std::string s) {
+double AverageVowelsPerWord(std::string vowelCounterPerWord) {
     // Returns the average number of vowels per word in the string. This function can make use of the IsVowel and NumWords helpers.
 }
 int main() {
