e306e8c4d88e089ffc12f9726673be0da0801434
Stuart <stuartjkirkham@gmail.com>
2022-08-28 10:26:09 -0600

    string analyzer homework

--- Week1/Day5/StringAnalyzer/StringAnalyzer/main.cpp
+++ Week1/Day5/StringAnalyzer/StringAnalyzer/main.cpp
@@ -12,13 +12,64 @@ bool IsTerminator(char terminatorCounter) {
     char periodCounter = '.';
     char questionCounter = '?';
     char exclamationCounter = '!';
-    if (terminatorCounter == periodCounter) {
+    // Searches for terminator punctuation.
+    if (terminatorCounter == periodCounter || terminatorCounter == questionCounter || terminatorCounter == exclamationCounter) {
         return true;
     }
-    else if (terminatorCounter == questionCounter) {
+    else {
+        return false;
+    }
+}
+
+bool IsPunctuation(char punctuationCounter) {
+    //The bool isPunctuation helps to not count those punctuation marks as a part of the letter count. Because commas aren't letters, but they're also not terminators of sentences.
+    //Returns whether or not the character is ., ?, !, or ,.
+     char periodCounter = '.';
+     char questionCounter = '?';
+     char exclamationCounter = '!';
+    char commaCounter = ',';
+    // This if statement searches for punctuation and returns true if found.
+     if (punctuationCounter == periodCounter || punctuationCounter == questionCounter || punctuationCounter == exclamationCounter || punctuationCounter == commaCounter) {
+         return true;
+     }
+     else {
+         return false;
+     }
+}
+bool IsSpace(char spaceChecker) {
+    char spaceCounter = ' ';
+    if (spaceChecker == spaceCounter){
         return true;
     }
-    else if (terminatorCounter == exclamationCounter) {
+    else {
+        return false;
+    }
+}
+bool IsVowel(char vowelChecker) {
+    // Returns whether or not the character is a vowel. For our purposes, always treat the character 'y' (and its uppercase version) as a vowel.
+    char aCounter = 'a';
+    char AupperCounter = 'A';
+    char eCounter = 'e';
+    char EupperCounter = 'E';
+    char iCounter = 'i';
+    char IupperCounter = 'I';
+    char oCounter = 'o';
+    char OupperCounter = 'O';
+    char uCounter = 'u';
+    char UupperCounter = 'U';
+    char yCounter = 'y';
+    char YupperCounter = 'Y';
+    // The following ifs statement string has any vowels; if it does, the boolean returns true.
+    if (vowelChecker == aCounter || vowelChecker == AupperCounter || vowelChecker == eCounter || vowelChecker == EupperCounter || vowelChecker == iCounter || vowelChecker == IupperCounter || vowelChecker == oCounter || vowelChecker == OupperCounter || vowelChecker == uCounter || vowelChecker == UupperCounter || vowelChecker == yCounter || vowelChecker == YupperCounter) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool IsConsonant(char consonantChecker) {
+    //Returns whether or not the character is a consonant.
+    if (!IsVowel(consonantChecker) && !IsPunctuation(consonantChecker) && !IsSpace(consonantChecker)) {
         return true;
     }
     else {
@@ -26,16 +77,6 @@ bool IsTerminator(char terminatorCounter) {
     }
 }
 /*
-bool IsPunctuation(char punctuationCounter) {
-    //The bool isPunctuation helps to not count those punctuation marks as a part of the letter count. Because commas aren't letters, but they're also not terminators of sentences.
-    //Returns whether or not the character is ., ?, !, or ,.
-}
-bool IsVowel(char vowelChecker) {
-    // Returns whether or not the character is a vowel. For our purposes, always treat the character 'y' (and its uppercase version) as a vowel.
-}
-bool IsConsonant(char consonantChecker) {
-    //Returns whether or not the character is a consonant.
-}
 int NumWords(std::string wordCounter) {
     //Returns the number of words in the string.
 }
@@ -60,23 +101,36 @@ int main() {
     // global variable section
     int punctuationTotal = 0;
     int vowelTotal = 0;
-    int spaceTotal = 0;
     int consonantTotal = 0;
     //
     std::cout << "Enter a string containing one or more sentences: \n";
     std::string input;
     std::getline( std::cin, input );
-    std::cout << input << "\n";
+//    std::cout << input << "\n";
     if (input == "done") {
         std::cout << "Goodbye.";
     }
     else {
         for (int i = 0; i<input.length(); i++) {
-            //std::cout << input[i];
+            //std::cout << input[i] << "\n"; testing string
             if (IsTerminator(input[i])) {
                 punctuationTotal=punctuationTotal+1;
             }
         }
+        for (int i = 0; i<input.length(); i++) {
+            //std::cout << input[i] << "\n"; testing string
+            if (IsVowel(input[i])) {
+                vowelTotal=vowelTotal+1;
+            }
+        }
+        for (int i = 0; i<input.length(); i++) {
+            //std::cout << input[i] << "\n"; testing string
+            if (IsConsonant(input[i])) {
+                consonantTotal=consonantTotal+1;
+            }
+        }
+        // std::cout << vowelTotal << "\n"; test for vowel number
+         std::cout << consonantTotal << "\n"; //test for consonant number
     }
     
     return 0;
