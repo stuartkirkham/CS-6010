c4dc3b92d915dd7cc495a8cb98ec5729b8f65001
Stuart <stuartjkirkham@gmail.com>
2022-08-29 12:53:21 -0600

    VectorHW

---
+++ Week1/Day5/StringAnalyzer/LetterHelpers.hpp
@@ -0,0 +1,17 @@
+//
+//  LetterHelpers.hpp
+//  StringAnalyzer
+//
+//  Created by Stuart Kirkham on 8/29/22.
+//
+
+#pragma once
+
+#include <stdio.h>
+
+bool IsTerminator(char terminatorCounter);
+bool IsPunctuation(char punctuationCounter);
+bool IsSpace(char spaceChecker);
+bool IsVowel(char vowelChecker);
+bool IsConsonant(char consonantChecker);
+
