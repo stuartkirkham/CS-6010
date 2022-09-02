b799e011920b19883419167f92c56b085370069b
Stuart <stuartjkirkham@gmail.com>
2022-09-02 15:17:57 -0600

    book analyzer homework

---
+++ Week3/Book Analyzer/Book Analyzer/main.cpp
@@ -0,0 +1,54 @@
+//
+//  main.cpp
+//  Book Analyzer
+//
+//  Created by Stuart Kirkham on 9/2/22.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+// Write a test file.
+//The title of the book
+//The author of the book
+//The total number of words in the file
+//The total number of characters in the file (excluding whitespace).
+//The shortest word in the book
+//The longest word in the book
+//The number of appearances, and locations of, the users key word (see below)
+
+int main(int argc, const char * argv[]) {
+    // Write a program to read in a book from file,
+    // compute the statistics defined below,
+    // and output the results to the terminal.
+    std::ofstream fout ("Testing.txt");
+    fout <<"Hello world! \n";
+    fout.flush(); // forces to write to file
+    fout.close();
+
+    fout.open("Testing.txt", std::ios_base::app);
+    fout << "A second line for fun \n";
+    fout.flush();
+    fout.close();
+    
+    //this is how you open and read from a file:
+    std::ifstream fin ("Testing.txt");
+    if (fin.fail()){
+        std::cout << "Nonexistent file! \n";
+        system("PAUSE");
+        return 0;
+    }
+    std::string singleWord;
+    std::vector<std::string> allWords;
+    while (fin >> singleWord) {
+        allWords.push_back(singleWord);
+    }
+
+
+
+
+
+    return 0;
+}
