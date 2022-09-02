097fcf4ba142f2144dfece96c88a09c8bdeaeb5f
Stuart <stuartjkirkham@gmail.com>
2022-09-02 11:34:54 -0600

    class examples

--- Week2/Day10/Pointer Class Examples/Pointer Class Examples/main.cpp
+++ Week2/Day10/Pointer Class Examples/Pointer Class Examples/main.cpp
@@ -13,6 +13,16 @@ void ModifyArray(int a[5]){
         a[i] += i;
     }
 }
+void PrintArray(int a[]/* can also use *a instead of a[] */, int size){ // how to print an array
+    for (int i = 0; i < size; i++){
+        std::cout << a[i];
+        // this is the same as *a = (1+i)
+    }
+}
+
+void printValAtPosition(int *a, int pos){
+    std::cout << *(a + pos*sizeof(int)/* can also use *(a+pos) which is easier to write if variable and pos are same datatype. if c++ doesnt know the datatype (like for a struct) you cant use the shorthand version. */);
+}
 
 void ReferenceBasedSwap(int& a, int&b){ // this is the better way to write a swap vs the NewSwap below using references.
     int temp = a;
@@ -33,7 +43,7 @@ int* ReturnAddress(int b) { // this is bad because the address is temporary, aft
 
 
 // what is const char *argv[]? it is a pointer to an array. basically an array of arrays or pointer of pointers.
-// pointers and arrays are interchangable.
+// pointers and arrays are interchangable. it is read as *argv[] because the parameters aren't predetermined.
 int main(int argc, const char * argv[]) {
     // create a C-style array and pass it to a function
     int a[5] = {1,2,3,4,5};
@@ -52,7 +62,36 @@ int main(int argc, const char * argv[]) {
     std::cout << "Value of name is " << name;
     std:: cout << " and the address is " << ptrToString;
     
+    //swap two numbers using pointers
+    int num3 = 6;
+    int num4 = 8;
+    //NewSwap (&num3, num4);
     
+    //c-style string
+    const char *myName = "Nemo";
     
+    //2d array
+    int table[5][5];
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j<5; j++){
+            table[i][j] = i+j;
+        }
+    }
+    
+    /*
+    can also write a 2d array as:
+     int table [] **1 is inferred here** [3] = { {2,3,4}};
+    
+    */
+    
+    // how to print out char* argv[]?
+    std::cout<< "\n";
+    if (argc > 0) {
+        //for (int i = 0; i < argc; i++){
+            //std::cout << argv[i];
+        std::string firstArgument = argv[0];
+        std::cout << firstArgument;
+        //}
+    }
     return 0;
 }
