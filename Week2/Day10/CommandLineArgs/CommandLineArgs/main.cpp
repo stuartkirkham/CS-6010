3e4bcc48ff4d6acc139515b2e6e4a8fc17664aac
Stuart <stuartjkirkham@gmail.com>
2022-09-02 13:21:56 -0600

    friday lab

---
+++ Week2/Day10/CommandLineArgs/CommandLineArgs/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  CommandLineArgs
+//
+//  Created by Stuart Kirkham on 9/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    if (argc > 0) {
+        //for (int i = 0; i < argc; i++){
+            //std::cout << argv[i];
+        std::string firstArgument = argv[0];
+        std::cout << firstArgument;
+        //}
+    }
+    return 0;
+}
