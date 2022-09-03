d55b21e3ce766ba03e4376c99f0f3808aa9be6cf
Stuart <stuartjkirkham@gmail.com>
2022-09-02 18:06:14 -0600

    Book Analyzer

--- Week3/Book Analyzer/Book Analyzer/main.cpp
+++ Week3/Book Analyzer/Book Analyzer/main.cpp
@@ -15,24 +15,42 @@
 //The title of the book
 void bookTitle(std::vector<std::string>allWords) {
     auto titleFound = find(allWords.begin(), allWords.end(), "Title:");
+    int indexTitle = titleFound - allWords.begin();
+    
+    //std::cout << indexTitle;
     auto authorFound = find(allWords.begin(), allWords.end(), "Author:");
-    std::string finalTitle = allWords.substr (titleFound, authorFound-titleFound);
-    for (int i = 0; i < allWords.size(); i++) {
-        if (titleFound != allWords.end()) {
-            for (int j = 0; j < allWords.size(); j++){
-                std::cout << titleFound[j];
-            }
-            std::cout << "\n";
-        }
-        
+    int indexAuthor = authorFound - allWords.begin();
+    //std::cout << indexAuthor;
+    for (int i = indexTitle + 1; i < indexAuthor; i++) {
+        std::cout<<allWords[i] << " ";
     }
+    std::cout << "\n";
 }
 //The author of the book
 void bookAuthor(std::vector<std::string>allWords){
-    
+    auto authorFound = find(allWords.begin(), allWords.end(), "Author:");
+    int indexAuthor = authorFound - allWords.begin();
+    //std::cout << indexTitle;
+    auto referenceFound = find(allWords.begin(), allWords.end(), "Release");
+    int indexReference = referenceFound - allWords.begin();
+    //std::cout << indexAuthor;
+    for (int i = indexAuthor + 1; i < indexReference; i++) {
+        std::cout<<allWords[i] << " ";
+    }
+    std::cout << "\n";
 }
 //The total number of words in the file
+void numWords(std::vector<std::string>allWords){
+    std::cout << allWords.size() << "\n";
+}
 //The total number of characters in the file (excluding whitespace).
+void numChars (std::vector<std::string> allWords){
+    int totalChars = 0;
+    for (int i = 0; i < allWords.size(); i++){
+        totalChars = totalChars + allWords[i].length();
+    }
+    std::cout << totalChars << "\n";
+}
 //The shortest word in the book
 //The longest word in the book
 //The number of appearances, and locations of, the users key word (see below)
@@ -45,12 +63,12 @@ int main(int argc, const char * argv[]) {
     // compute the statistics defined below,
     // and output the results to the terminal.
    
-    /*std::ofstream fout ("Testing.txt");
+    /* std::ofstream fout ("Testing.txt");
     fout <<"Hello world! \n";
     fout.flush(); // forces to write to file
     fout.close();
 
-    fout.open("Testing.txt", std::ios_base::app);
+    fout.open("Test.txt", std::ios_base::app);
     fout << "A second line for fun \n";
     fout.flush();
     fout.close();
@@ -68,6 +86,13 @@ int main(int argc, const char * argv[]) {
     while (fin >> singleWord) {
         allWords.push_back(singleWord);
     }
+    std::cout << "Title: ";
     bookTitle(allWords);
+    std::cout << "Author: ";
+    bookAuthor(allWords);
+    std::cout <<"Number of words: ";
+    numWords(allWords);
+    std::cout <<"Number of characters: ";
+    numChars(allWords);
     return 0;
 }
